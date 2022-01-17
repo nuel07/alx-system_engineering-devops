b57bdded063b82385a3a713e35147a4ee4a43b63
nuel07 <yehanganep@gmail.com>
2022-01-17 03:37:52 -0800

    corrections

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,80 @@
 #creating a custom HTTP header response
 
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
--> package {'nginx':
-  ensure => present,
+
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
--> file_line { 'header line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
+
+# install nginx
+package { 'nginx':
+  ensure     => 'installed',
 }
--> exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
-}line   => "
+
+# allow HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# create index file
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+# create index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# add redirection and error page
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        add_header X-Served-By \$hostname;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
+# restart nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# start service nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
