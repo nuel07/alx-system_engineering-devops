9cfe473e05a44904defc760f2ce0ec731bfb18cc
nuel07 <yehanganep@gmail.com>
2022-02-06 12:14:05 -0800

    task 2 correction

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ export data to JSON fomat"""
 import json
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "user/{}".format(user_id)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
     with open("{}.json".format(user_id), 'w') as jsonfile:
