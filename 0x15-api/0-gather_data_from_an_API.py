a1cacfbda5d1c23f991ca2f9b671ea300fa1f732
nuel07 <yehanganep@gmail.com>
2022-02-06 11:17:32 -0800

    task 0 fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
-    tdlst = requests.get(url + 'tdlst', params={"userId":user_id}).json()
+    todos = requests.get(url + 'todos', params={"userId":user_id}).json()
     complete = [tsk.get("title") for tsk in tdlst if tsk.get("completed") is True]
     print("Employee {} is done with tasks {}/{}".format(user.get("name"),
                                                         len(complete), len(tdlst)))
