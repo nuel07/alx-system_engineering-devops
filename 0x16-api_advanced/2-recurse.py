c592e9a1284bb56b6d89882964e15edbede297ae
nuel07 <yehanganep@gmail.com>
2022-02-15 10:02:46 -0800

    task 2-recurse pep fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -17,7 +17,8 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {"User-Agent": "ubuntu/20.04"}
     params = {"after":after, "count":count, "limit":10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
     if response.status_code == 404:
         return None
