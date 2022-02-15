887cefb0a3c897088f2ae4f33f0c254b4cdc0203
nuel07 <yehanganep@gmail.com>
2022-02-15 08:43:24 -0800

    tests directory

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' Queries given API for hottest subreddits '''
+
+import requests
+
+
+def top_ten(subreddit):
+    ''' Prints titles of 10 hottest posts on given subreddit '''
+    
